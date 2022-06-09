ef76dc3bd8d80516acfaaaebc9f3ad96c48535f6
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-06-10 00:48:02 +0300

    commit tree 8b6f401ad2330468d11dfd5e3bb7edf1b6e2656e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(driver, power) {
+  return driver.filter(
+    (theMatch) => theMatch.toLowerCase() === power.toLowerCase()
+  );
+}
+
+function fuzzyMatch(driver, testString) {
+  return driver.filter(
+    (theMatch) => theMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
+
+function matchName(driver,  powerName) {
+ return driver.filter((record) => record.name ===  powerName);
+ }
